2b63e87d368cf67916c5d6306a2ed51ed9a67e12
Elia Bardashevich <ebardashevich@gmail.com>
2016-05-25 09:35:09 +0100

    .new

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,7 +8,7 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant = Restaurant.create(restaurant_params)
+    @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
       redirect_to restaurants_path
     else
