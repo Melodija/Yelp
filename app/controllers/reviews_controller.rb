e77503794343872a83d6d05b3e8c12b70ad05fd5
Elia Bardashevich <ebardashevich@gmail.com>
2016-05-27 12:02:28 +0100

    tests passing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,16 +6,18 @@ class ReviewsController < ApplicationController
   end
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.build_review params, current_user
-    if @review.saveredirect_to restaurants_path
-    else
-      if @review.errors[:user]
-
-    end
-
-    end
-    @restaurant.reviews.create(review_params)
-    redirect_to '/restaurants'
+    @review = @restaurant.reviews.build_with_user(review_params, current_user)
+      if @review.save
+        redirect_to restaurants_path
+        else
+          if @review.errors[:user]
+            redirect_to restaurants_path, alert: 'You have already reviewed this restaurant'
+          else
+            render :new
+        end
+      end
+      @restaurant.reviews.create(review_params)
+      # redirect_to restaurants_path
   end
   def review_params
    params.require(:review).permit(:thoughts, :rating)
